c574b793f417829c7e0ed14e399962ea3eaa9939
Benjamin Jacobs <benjammin1100@gmail.com>
2018-07-24 15:35:24 -0600

    Add flask Add basic layout

--- src/blueweather/__init__.py
+++ src/blueweather/__init__.py
@@ -1,2 +1,35 @@
-def main():
-    print("Hello World!")
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+# Make the WSGI interface available at the top level so wfastcgi can get it
+wsgi_app = app.wsgi_app
+
+breadcrumb_layout = [
+    {
+        'name': 'Home',
+        'url': '/dashboard'
+    },
+    {
+        'name': 'Dashboard'
+    }
+]
+
+# server/
+@app.route('/')
+@app.route('/dashboard')
+def hello():
+    return render_template('dashboard.html', breadcrumbs=breadcrumb_layout)
+
+
+def main(debug=False):
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5000'))
+    except ValueError:
+        PORT = 5000
+    app.run(HOST, PORT, debug)
+
+
+if __name__ == '__main__':
+    main(True)
