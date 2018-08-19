f1d1af9fc1927a13ca8079430bdcb35ee0b3252e
Benjamin Jacobs <benjammin1100@gmail.com>
2018-08-18 22:07:30 -0600

    Add support for current weather in weather page

--- src/blueweather/web/main/util.py
+++ src/blueweather/web/main/util.py
@@ -1,9 +1,15 @@
 import os
 
-from blueweather import variables
-
 import flask
 import flask_login
+
+from blueweather import variables
+
+_weather = 'clear'
+
+def setWeatherIcon(weather: str):
+    global _weather
+    _weather = weather
 
 def get_bread_crumb(url: str):
     if url == variables.config.web.home_page:
@@ -37,6 +43,42 @@ def get_bread_crumb(url: str):
     return breadcrumb
 
 
+def get_weather_icon(weather: str) -> str:
+    """
+    Get the ``weather-icons`` icon for the current weather. the options are
+
+    ``clear, fog, rain, sleet, snow-wind, thunderstorm, cloudy-high, cloudy,
+    hail, rain-mix, sleet-storm, sprinkle, windy, cloudy-gusts,
+    haze, rain-wind, snow, storm-showers, cloudy-windy, lightning, showers,
+    snow-thunderstorm, partly-cloudy``
+    """
+    from datetime import datetime
+
+    hour = datetime.now().hour
+
+    if hour > 20 or hour < 7:
+        # It's the night time
+        specials = {
+            'clear': 'wi wi-fw wi-night-clear',
+            'fog': 'wi wi-fw wi-night-fog',
+            'windy': 'wi wi-fw wi-windy',
+        }
+        if weather in specials:
+            return specials[weather]
+
+        return 'wi wi-fw wi-night-alt-{}'.format(weather)
+
+    specials = {
+        'clear': 'wi wi-fw wi-day-sunny',
+        'partly-cloudy': 'wi wi-fw wi-day-sunny-overcast'
+    }
+    if weather in specials:
+        return specials[weather]
+
+    return 'wi wi-fw wi-day-{}'.format(weather)
+
+current = 0
+
 def get_side_bar(route: str):
 
     # In the future, I would like to more dynamically generate the sidebar data
@@ -51,7 +93,7 @@ def get_side_bar(route: str):
         },
         {
             'name': 'Weather',
-            'icon': 'wi wi-day-sunny fa-fw',
+            'icon': get_weather_icon(_weather),
             'url': flask.url_for('main.data'),
             'active': route == 'main.data'
         }
